11a68e4c887dc3bc4dd2bb27aade850e9097c823
roach <roach@roach-MS-7845.(none)>
2016-03-28 15:57:27 -0300

    commit final revizado

--- README.md
+++ README.md
@@ -1,34 +1,11 @@
 # Mini_2016
-Implementation of the digital back-end using ROACH technology into the MINI- Radio Telescope.
+Implementation of the digital back-end using ROACH technology into the MINI- Radio Telescope. In this contribution we incorporate an async reset signal, in order to trigger a new accumulation using the gpio in the ROACH. 
+In the ROACH-I for cabling convenience, two of the GPIO pins are also wired to SMA connectors. We are using the J11 sma connector. The GPIO input takes 0-3.3V, ranging from DC to ~10MHz.
 
-Several scripts have been written, the latest version are online here in the repository. Three python scripts are necessary for test this project.
+Several scripts have been written, the latest version are online here in the repository. Three python scripts are necessary for test this project:
 
-#1) Two spectrometers with one bram per band: 
+1) spectrometer_mini.py 
+2) calibration.py
+3) 2sb_mini.py 
 
-How to run this script?:
-
-./spec12_32b_2bram.py 192.168.1.11 -g 262143 -l 65536
-
-Gain = 2^{18} - 1 = 262143
-
-Accumulation length = 65536 very long!
-
-#2) The Calibration script:
-
-to run it:
-
-./spectrometer_64bits_float_s12_dif_ed_sin_acc_data_upper_low2_dctrl.py 192.168.1.11  -g 0x0080000 -b cal_spectrometer_2048ch_2014_Jul_15_0203.bof
-
-Gain = 0x0080000 (hex) = 524288 (Decimal).
-
-#3) DSSS script for SRR measurement:
-
-To run this script:
-
-./spectrometer_32bits_1brampsb_s12_SRR_measurement.py 192.168.1.11 -g 262143 
--l 65536 -b minispec_1ghz_1bram_u_2015_Sep_04_1658.bof
-
-Gain = 2^{18} - 1 = 262143
-
-Accumulation length = 65536 very long!
-
+The boffiles are available in the bof folder.
---
+++ code/spectrometer_mini.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+'''
+This script configuring the wideband spectrometer and plotting the received data using the Python KATCP library along with the katcp_wrapper distributed in the corr package. 
+Designed for use with MINI Telescope and ROACH platforms.\n
+
+You need to have KATCP and CORR installed. Get them from http://pypi.python.org/pypi/katcp and http://casper.berkeley.edu/svn/trunk/projects/packetized_correlator/corr-0.4.0/
+
+To run this script:
+./spectrometer_mini.py 192.168.1.11 -g 262143 -l 65536
+
+Gain = 2^18 - 1 = 262143 
+accumulation length = 65536 very long!
+
+\nAuthor: Andres, May 2015.
+'''
+
+
+import corr, time, numpy, struct, sys, logging, math, Gnuplot, Gnuplot.funcutils, array
+from math import *
+
+
+bitstream = 'minispec_chopper_2016_Mar_23_1621.bof'
+
+
+katcp_port=7147
+
+def exit_fail():
+    print 'FAILURE DETECTED. Log entries:\n',lh.printMessages()
+    try:
+        fpga.stop()
+    except: pass
+    raise
+    exit()
+
+def exit_clean():
+    try:
+        fpga.stop()
+    except: pass
+    exit()
+
+def get_data():
+    #get the data...    
+    acc_n = fpga.read_uint('acc_cnt')
+    
+    bla = fpga.read_uint('gpio_state')
+    print bla
+
+    a_0l=struct.unpack('>2048I',fpga.read('bram0',2048*4,0))
+#
+    a_0m=struct.unpack('>2048I',fpga.read('bram1',2048*4,0))
+
+
+    interleave_a=[]
+    interleave_b=[]
+    interleave_log=[]        
+    interleave_log_b=[]
+
+    for i in range(2048):
+        interleave_a.append(float(float(a_0l[i])+1))
+        interleave_b.append(float(float(a_0m[i])+1))
+
+
+    for k in range(4*512):
+        interleave_log.append(10*log10(interleave_a[k]))
+        interleave_log_b.append(10*log10(interleave_b[k]))
+
+    return acc_n, interleave_a, interleave_log, interleave_log_b
+
+def continuous_plot(fpga):
+    ok=1
+    bw=trunc(fpga.est_brd_clk())*4
+    acc_n, interleave_a, interleave_log, interleave_log_b = get_data()
+    g0.clear()    
+    g0.title('ADC0 acc:'+str(acc_n)+' | Bw= '+str(bw)+' MHz')
+    g0.ylabel('Power AU (dB)')
+    g0('set style data linespoints')
+    g0('set yrange [0:100]')
+    g0('set xrange [-50:2098]')
+    g0('set ytics 5')
+    g0('set xtics 256')
+    g0('set grid y')
+    g0('set grid x')
+
+    g1.clear()
+    g1.title('ADC1 acc:'+str(acc_n)+' | Bw= '+str(bw)+' MHz')
+    g1.ylabel('Power AU (dB)')
+    g1('set style data linespoints')
+    g1('set yrange [0:100]')
+    g1('set xrange [-50:2098]')
+    g1('set ytics 5')
+    g1('set xtics 256')
+    g1('set grid y')
+    g1('set grid x')
+
+    while ok==1 :
+        acc_n, interleave_a, interleave_log, interleave_log_b = get_data()
+
+        g0.plot(interleave_log)
+        time.sleep(0.1)
+        g1.plot(interleave_log_b)
+
+
+
+
+
+################   START OF MAIN   ################
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+
+
+    p = OptionParser()
+    p.set_usage('spectrometer.py <ROACH_HOSTNAME_or_IP> [options]')
+    p.set_description(__doc__)
+    p.add_option('-l', '--acc_len', dest='acc_len', type='int',default=2*(2**28)/2048,
+        help='Set the number of vectors to accumulate between dumps. default is 2*(2^28)/2048, or just under 2 seconds.')
+    p.add_option('-g', '--gain', dest='gain', type='int',default=0x00001000,
+        help='Set the digital gain (6bit quantisation scalar). Default is 0xffffffff (max), good for wideband noise. Set lower for CW tones.')
+    p.add_option('-s', '--skip', dest='skip', action='store_true',
+        help='Skip reprogramming the FPGA and configuring EQ.')
+    p.add_option('-b', '--bof', dest='boffile',type='str', default='minispec_chopper_2016_Mar_23_1621.bof',
+        help='Specify the bof file to load')
+    opts, args = p.parse_args(sys.argv[1:])
+
+    if args==[]:
+        print 'Please specify a ROACH board. Run with the -h flag to see all options.\nExiting.'
+        exit()
+    else:
+        roach = args[0] 
+    if opts.boffile != '':
+        bitstream = opts.boffile
+
+try:
+    loggers = []
+    lh=corr.log_handlers.DebugLogHandler()
+    logger = logging.getLogger(roach)
+    logger.addHandler(lh)
+    logger.setLevel(10)
+
+    print('Connecting to server %s on port %i... '%(roach,katcp_port)),
+    fpga = corr.katcp_wrapper.FpgaClient(roach, katcp_port, timeout=10,logger=logger)
+    time.sleep(1)
+
+    if fpga.is_connected():
+        print 'ok\n'
+    else:
+        print 'ERROR connecting to server %s on port %i.\n'%(roach,katcp_port)
+        exit_fail()
+
+    print '------------------------'
+    print 'Programming FPGA with %s...' %bitstream,
+    if not opts.skip:
+        fpga.progdev(bitstream)
+        print 'done'
+    else:
+        print 'Skipped.'
+#
+    print 'waiting 3 seconds...'
+    time.sleep(3)
+#
+    print 'Configuring FFT shift register...',
+    fpga.write('shift_ctrl','\x00\x00\x0f\xff')
+    print 'done'
+#
+    print 'Configuring accumulation period...',
+    fpga.write_int('acc_len',opts.acc_len)
+    print 'done'
+#
+    print 'Resetting counters...',
+    fpga.write_int('cnt_rst',1) 
+    fpga.write_int('cnt_rst',0) 
+    print 'done'
+#
+    print 'Setting digital gain of all channels to %i...'%opts.gain,
+    if not opts.skip:
+        fpga.write_int('gain',opts.gain) #write the same gain for all inputs, all channels
+        print 'done'
+    else:   
+        print 'Skipped.'
+    
+    #set up the figure with a subplot to be plotted
+    g0 = Gnuplot.Gnuplot(debug=0)
+    g1 = Gnuplot.Gnuplot(debug=0)
+
+    continuous_plot(fpga)	
+
+    print 'Plot started.'
+
+except KeyboardInterrupt:
+    exit_clean()
+except:
+    exit_fail()
+
+exit_clean()
+
